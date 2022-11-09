fa63e04b126c0aca71828d094dee2e3ff2985b31
Anderson da Mata <anderson.daamata@gmail.com>
2022-11-09 02:00:24 -0300

    Finalizado o front da tela de login projeto angular

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginModel } from 'src/app/models/loginModel';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required]]
+    });
+  }
+
+  submitLogin() {
+    debugger
+    var dadosLogin = this.loginForm.getRawValue() as LoginModel;
   }
 
 }
